c77afcad189e0fafa3293ef04c4b6af9aef5d947
faddat <faddat@gmail.com>
2016-05-17 10:35:01 +0700

    Re-organized and moved more things to goroutines for better clarity.

--- makeimg.go
+++ makeimg.go
@@ -3,61 +3,40 @@ package main
 import (
 	"fmt"
 	"github.com/codeskyblue/go-sh"
-	"os"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"github.com/olebedev/config"
-
 )
 
 func main() {
-	//YAML IS EATEN HERE
-	cfg, err := config.ParseYaml(yamlString)
-	host, err := cfg.String("development.database.host")
-	var t int = 0
+	//Prompts the user to ask which configuration they'd like to use
+	var confignumber int = 0
 	fmt.Println("Enter the number of the board you want to build for")
 	files, err := ioutil.ReadDir("./boards")
 	if err != nil {
 	log.Fatal(err)
 	return err
 	}
-
-
 	//LISTS  THE CONFIG FILES AND PROMPTS USER TO CHOOSE A CONFIG NUMBER
 	fmt.Println("TYPE THE NUMBER OF THE ROOTFS YOU WANT TO USE. This will be made dynamic soon. ")
 	for _, e := {
-		fmt.Println(t, e)
+		fmt.Println(confignumber, e)
 	}
-
 	for _, file := range files {
 		t = t + 1
 		fmt.Println(t, " ", file.Name())
 		var e []t
 	}
 
-	//BUILDS OUT THE FOLDER STRUCTURE NEEDED FOR BUILDING
-	session := sh.NewSession()
-	session.ShowCMD = true
-	session.Command("mkdir /builds && echo [ OK ] || 'echo [ OK }' ")
-	session.Command("mkdir /builds/boards || echo '/builds/boards already present [ OK ]' )
-	session.Command("mkdir /builds/images || echo '[ OK ]' ")
-	session.Command("mkdir /builds/kernels")
-	session.Command("mkdir /builds/u-boots")
-	session.Command("mkdir /builds/rootfses")
-	session.Command("mkdir /buidls/u-boots")
-	session.SetDir("/builds")
-	session.Command("echo hello")
-	session.Command("git clone ")
-	return err
+	go FolderStructure
+	go ParseYamlFile
+
+
 
 	//Goroutines fetch the neeeded files
 	go Ubuntu()
 	go Debian()
 	go Arch()
 	go Pecovnik()
-
-
 	}
 
 	app.Run(os.Args)
@@ -82,9 +61,30 @@ func Arch() {
 	sh.Command("wget http://sg2.mirror.archlinuxarm.org/os/ArchLinuxARM-armv7-latest.tar.gz")
 }
 
-//Downloads one of Uncle Igor's ROMs to use the structure he's so kindly built for us, and mounts the ROM Image
+//Downloads and mounts one of Uncle Igor's ROMs to use the structure he's so kindly built for us, and mounts the ROM Image
 func Pecovnik(){
 	sh.Command("wget http://mirror.igorpecovnik.com/Armbian_5.10_Orangepih3_Debian_jessie_3.4.112.7z")
+	sh.Command("")
 }
+
+func ParseYamlFile(filename string) (*Config, error)
+
+
+//BUILDS OUT THE FOLDER STRUCTURE NEEDED FOR BUILDING
+func FolderStructure
+session := sh.NewSession()
+session.ShowCMD = true
+session.Command("mkdir /builds && echo [ OK ] || 'echo [ OK }' ")
+session.Command("mkdir /builds/boards || echo '/builds/boards already present [ OK ]' )
+session.Command("mkdir /builds/images || echo '[ OK ]' ")
+session.Command("mkdir /builds/kernels")
+session.Command("mkdir /builds/u-boots")
+session.Command("mkdir /builds/rootfses")
+session.Command("mkdir /buidls/u-boots")
+session.SetDir("/builds")
+session.Command("echo hello")
+session.Command("git clone ")
+return err
+
 
 
