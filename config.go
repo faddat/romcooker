df544682ed494412edd569bb88815d868d11fd80
faddat <faddat@gmail.com>
2016-05-18 06:36:26 +0700

    round 2?

--- config.go
+++ config.go
@@ -1,4 +1,32 @@
 package main
 
-func main() {
+import (
+	"github.com/olebedev/config"
+	"flag"
+)
+
+type Configuration interface {
+	ParseYamlFile(filename string) (*config.Config, error)
+}
+
+/*
+Initialize Configuration
+Defines some configuration options in a global variable
+ */
+var CFG *config.Config
+
+func loadConfig() {
+	var configPath string
+	flag.StringVar(&configPath, "config", "", "The path to configuration")
+	flag.Parse()
+	if configPath == "" {
+		Error.Panic("No config specified")
+	}
+	config, err := config.ParseYamlFile(configPath)
+	if err != nil {
+		Error.Println(err.Error())
+		return
+	}
+	Info.Println("config loaded from " + configPath)
+	CFG = config.Flag()
 }
